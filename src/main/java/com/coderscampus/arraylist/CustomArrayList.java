d2ff9a572256ba90e193bf538cd87d1915b9fde6
Ben Woodard <benjaminwoodard93@gmail.com>
2023-11-28 14:07:23 -0500

    refactored doubleArrayLength and add methods

--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -4,13 +4,9 @@ public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
 	private int itemsIndex = 0;
 
-	// Add method
 	@Override
 	public boolean add(T item) {
-		if (itemsIndex == items.length) {
-			items = doubleArrayLength();
-		}
-
+		doubleArrayLength();
 		items[itemsIndex] = item;
 		itemsIndex++;
 		return true;
@@ -28,11 +24,14 @@ public class CustomArrayList<T> implements CustomList<T> {
 	}
 
 	public Object[] doubleArrayLength() {
-		Object[] newItems = new Object[items.length * 2];
-		for (int i = 0; i < items.length; i++) {
-			newItems[i] = items[i];
+		if (itemsIndex == items.length) {
+			Object[] newItems = new Object[items.length * 2];
+			for (int i = 0; i < items.length; i++) {
+				newItems[i] = items[i];
+			}
+			items = newItems;
 		}
-		return newItems;
+		return items;
 	}
 
 }
